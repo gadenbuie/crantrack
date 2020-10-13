86fc1933c2923593a48006094b82b194380d9829
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-10-13 14:27:48 +0000

    initial commit

---
+++ snapshot.R
@@ -0,0 +1,15 @@
+library(git2rdata)
+
+cran_snapshot <- cransays::take_snapshot()
+
+snapshot_time <- strftime(cran_snapshot[[1, "snapshot_time"]], "[snapshot] %F %T UTC", tz = "UTC")
+
+repo <- repository()
+write_vc(
+  as.data.frame(cran_snapshot), 
+  file = "cran_snapshot", 
+  root = repo, 
+  stage = TRUE, 
+  sorting = c("submission_time", "package")
+)
+commit(message = snapshot_time)
